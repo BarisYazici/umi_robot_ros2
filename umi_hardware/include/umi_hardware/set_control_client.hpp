eae44b3b10a61f2a3b739823c45e3aa5ceb57aee
Baris Yazici <barisyazici@sabanciuniv.edu>
2024-04-25 20:46:03 +0200

    adapt using the rostopics for hardware communication

--- umi_hardware/include/umi_hardware/set_control_client.hpp
+++ umi_hardware/include/umi_hardware/set_control_client.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "umi_robot_msgs/srv/control_servo.hpp"
+#include "umi_robot_msgs/msg/joint_states.hpp"
 #include "rclcpp/rclcpp.hpp"
+
 #include <iostream>
 
 class ServoControlClient : public rclcpp::Node
@@ -9,32 +10,21 @@ class ServoControlClient : public rclcpp::Node
 public:
     ServoControlClient(const rclcpp::NodeOptions& options) : rclcpp::Node("servo_control_client", options)
     {
-        client_ = create_client<umi_robot_msgs::srv::ControlServo>("/set_servo_angle");
-        while (!client_->wait_for_service(std::chrono::seconds(1)))
-        {
-            if (!rclcpp::ok())
-            {
-                RCLCPP_ERROR(get_logger(), "Interrupted while waiting for the service. Exiting.");
-                return;
-            }
-            RCLCPP_INFO(get_logger(), "service not available, waiting again...");
-        }
+        publisher_ = this->create_publisher<umi_robot_msgs::msg::JointStates>("/set_servo_angle", 1000);
     }
 
     void send_command(const std::vector<double>& angles)
     {
-        auto request = std::make_shared<umi_robot_msgs::srv::ControlServo::Request>();
+        auto message = umi_robot_msgs::msg::JointStates();
 
-        request->motor_1 = angles[0];
-        request->motor_2 = angles[1];
-        request->motor_3 = angles[2];
-        request->motor_4 = angles[3];
+        message.motor_1 = angles[0];
+        message.motor_2 = angles[1];
+        message.motor_3 = angles[2];
+        message.motor_4 = angles[3];
 
-        auto result_future = client_->async_send_request(request);
-        // blocking wait
-        // result_future.wait();
+        publisher_->publish(message);
     }
 
 private:
-    rclcpp::Client<umi_robot_msgs::srv::ControlServo>::SharedPtr client_;
+    rclcpp::Publisher<umi_robot_msgs::msg::JointStates>::SharedPtr publisher_;
 };
